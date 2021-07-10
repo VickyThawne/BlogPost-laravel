ded2164e3a18669fcc3fa9521da0c9989af5113f
VickyThawne <82896801+VickyThawne@users.noreply.github.com>
2021-07-10 14:54:13 +0530

    added categoryFactory and eloquent between post and category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+    protected $with = ['author', 'category'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
     
 }

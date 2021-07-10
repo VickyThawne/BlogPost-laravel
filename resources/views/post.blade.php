3ed7e3fa90dd0fdbb879a38c7aaa9050fc074e3b
VickyThawne <82896801+VickyThawne@users.noreply.github.com>
2021-07-10 14:59:06 +0530

    updating blade view to show author and category of post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,14 @@
 @section('content')
     <article>
         <h1>{{$post->title}}</h1>
-        
+
+        <p>
+            By <a href='author/{{$post->author->userName}}'>{{$post->author->name}} </a> in <a href="categories/{{$post->category->slug}}">{{ $post->category->name}}</a>
+        </p>    
         <div>
+            <p>
             {!!$post->body!!}
+            </p>
         </div>
     </article>
 

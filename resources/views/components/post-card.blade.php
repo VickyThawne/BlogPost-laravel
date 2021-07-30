4f301a0df280631e0edeab1a8703039cb06f9851
VickyThawne <82896801+VickyThawne@users.noreply.github.com>
2021-07-30 23:08:36 +0530

    adding basic neccessary components for clean up code

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,5 +1,8 @@
+@props(['post'])
+
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    {{$attributes->merge(['class' =>"transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"])}}>
+
     <div class="py-6 px-5">
         <div>
             <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
@@ -8,9 +11,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="categories/{{$post->category->slug}} "
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{$post->category->name}}</a>
+                    <x-category-label :category="$post->category" />
                 </div>
 
                 <div class="mt-4">
@@ -34,8 +35,11 @@
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
-                        <h5 class="font-bold"><a href="author/{{$post->author->userName}}">{{$post->author->name}}</a></h5>
-                        <h6>Mascot at Laracasts</h6>
+                        <h5 class="font-bold">
+                            <a href="/?author={{$post->author->userName}}">
+                                {{$post->author->name}}
+                            </a>
+                        </h5>
                     </div>
                 </div>
 

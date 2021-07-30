5e8592d7a0b07614e081795256b4ccc4862cd3ea
VickyThawne <82896801+VickyThawne@users.noreply.github.com>
2021-07-30 23:13:29 +0530

    Controller for post added and filter process for searching a blog.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use DeepCopy\Filter\Filter;
+use League\CommonMark\Normalizer\SlugNormalizer;
 
 class Post extends Model
 {
@@ -12,6 +14,26 @@ class Post extends Model
 
     protected $guarded = [];
     protected $with = ['author', 'category'];
+    
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function($query, $search){
+            $query->where(fn($query) =>
+                $query->where('title','like','%'. $search .'%')
+                ->orWhere('body','like','%'. $search .'%')
+            );
+        });
+        
+        $query->when($filters['category'] ?? false, function($query, $category){
+            $query->whereHas('category', fn($query) => 
+                $query->where("slug", $category));
+        });
+
+        $query->when($filters['author'] ?? false, function($query, $author){
+            $query->whereHas('author', fn($query) => 
+                $query->where("userName", $author));
+        });
+    }
 
     public function category(){
         return $this->belongsTo(Category::class);
